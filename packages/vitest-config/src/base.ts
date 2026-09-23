0a7777d9532089894f024646984c66ccd51e0d08
Manish Kumar <makumar.work@gmail.com>
2026-09-23 15:23:11 +0200

    remove unused files

--- packages/vitest-config/src/base.ts
+++ packages/vitest-config/src/base.ts
@@ -1,9 +1,5 @@
 import type { TestUserConfig } from 'vitest/config';
 
 export const baseTestConfig = {
-  coverage: {
-    provider: 'v8',
-    reportsDirectory: 'coverage',
-  },
   globals: true,
 } as const satisfies TestUserConfig;
